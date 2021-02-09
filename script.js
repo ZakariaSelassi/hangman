1648223299c996a831f3a8d99d173189bf9d897a
zakaria <zakariaselassi@gmail.com>
2021-02-09 13:44:34 +0100

    etape 4

--- script.js
+++ script.js
@@ -14,17 +14,17 @@
     // Etape 1 : récupérer un mot aléatoire dans un tableau.
     randomWords = guessWord[Math.floor(Math.random() * guessWord.length)];
     alert(randomWords);
-
+    let arrayWord = randomWords.split("")
+    let span;
     // Etape 2 : remplacer lettre par underscore 
     for (let i = 0; i < randomWords.length; i++) {
         //secretWord.push('_');
-        let span = document.createElement("span");
+        span = document.createElement("span");
         span.setAttribute("class", "myspan");
         span.innerText = "_";
         document.getElementById("letterWord").appendChild(span).style.letterSpacing = "10px";
     }
     // Etape 3 : Verifiez si la lettre est dans le tableau
-    // checks if letter matches one or more of letters in the word javascript
     document.getElementById("lettre").addEventListener("input", function() {
         let lettre = document.getElementById("lettre");
         //let l = lettre.value;
@@ -39,6 +39,27 @@
             return console.log("wrong letter ");
         }
     });
+
+    // Etape 4 : display letter match to word
+    document.getElementById("btn").addEventListener("click", function() {
+        let p = lettre.value;
+        console.log(lettre.value);
+        let s = document.querySelectorAll(".myspan");
+        console.log(s);
+        for (let i = 0; i < randomWords.length; i++) {
+            if (p === arrayWord[i]) {
+                s[i].innerHTML = p;
+                console.log(s[i]);
+            }
+            /*else {
+                           s[i].innerHTML = '_';
+
+                       }*/
+
+            document.getElementById("letterWord").appendChild(span);
+        }
+    });
+
 
 
 
