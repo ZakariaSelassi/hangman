e04a58d9410dd27e9c6480f43580d41c2901e9ff
zakaria <zakariaselassi@gmail.com>
2021-02-10 14:04:16 +0100

    compteur fixe

--- script.js
+++ script.js
@@ -9,6 +9,8 @@
         "coder",
         "asynchronous"
     ];
+
+    let cpt = 0;
     let secretWord = [];
     let life = 5;
     let stock = document.createElement("p");
@@ -22,72 +24,57 @@
     let span;
     // Etape 2 : remplacer lettre par underscore 
     for (let i = 0; i < randomWords.length; i++) {
-        secretWord.push('_');
         span = document.createElement("span");
         span.setAttribute("class", "myspan");
-        span.innerText = secretWord[i];
+        span.innerText = "_";
         document.getElementById("letterWord").appendChild(span).style.letterSpacing = "10px";
     }
     // Etape 3 : Verifiez si la lettre est dans le tableau
     document.getElementById("lettre").addEventListener("input", function() {
         let lettre = document.getElementById("lettre");
-        //let l = lettre.value;
-        // console.log(l);
-        console.log(typeof randomWords);
-
-        // console.log(randomWords.indexOf(lettre.value));
-        // doit return true si l'element recherche est dans la chaine.
-        if (randomWords.indexOf(lettre.value) > -1 && lettre.value != "") {
-            return console.log("ok");
-        } else if (life > 0) {
-            life = life - 1;
-            stock.innerHTML = "life remaining : " + life;
-            return console.log("life point :", life);
-        } else if (life == 0) {
-            alert("You lose ! Try it again");
-            return location.reload();
+        for (let i = 0; i < randomWords.length; i++) {
+            if (randomWords.indexOf(lettre.value) > -1 && lettre.value != "") {
+                return console.log("ok");
+            } else if (life > 0) {
+                life = life - 1;
+                stock.innerHTML = "life remaining : " + life;
+                return console.log("life point :", life);
+            } else if (life == 0) {
+                alert("You lose ! Try it again");
+                return location.reload();
+            }
         }
-
-
 
     });
 
     // Etape 4 : display letter match to word
     document.getElementById("btn").addEventListener("click", function() {
         let p = lettre.value;
-        console.log(lettre.value);
+
         let s = document.querySelectorAll(".myspan");
-        console.log(secretWord);
-        console.log(arrayWord);
         for (let i = 0; i < randomWords.length; i++) {
             if (p === arrayWord[i]) {
                 s[i].innerHTML = p;
-                console.log(s[i]);
+                cpt++;
+                console.log(s);
+                console.log(cpt, arrayWord.length);
+                if (cpt == arrayWord.length) {
+                    alert("win");
+                }
             }
-            // Compare 2 tableau si ils sont egals afficher win 
-            if (secretWord.equals([arrayWord])) {
-                alert("win");
-            }
-
         }
+
+
+
 
 
     });
 
+    function win(cpt) {
+        if (cpt === arrayWord.length) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
+    }
 
 
 })();
